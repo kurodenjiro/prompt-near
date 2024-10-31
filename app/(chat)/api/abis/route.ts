deefcceeaa7353f002e86304c6235a38da011f15
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-31 16:41:32 +0700

    update

--- app/(chat)/api/abis/route.ts
+++ app/(chat)/api/abis/route.ts
@@ -3,14 +3,26 @@ export const maxDuration = 300;
 
 export async function GET(req: NextRequest) {
   const account = req.nextUrl.searchParams.get('account') || '';
+  const chain = req.nextUrl.searchParams.get('chain') || '';
+  const network = req.nextUrl.searchParams.get('network') || '';
 
   try {
-    const response = await fetch(
-      `https://api.nearblocks.io/v1/account/${account}/contract/parse`
-    );
+    if (network == 'near' && chain == 'mainnet') {
+      const response = await fetch(
+        `https://api.nearblocks.io/v1/account/${account}/contract/parse`
+      );
 
-    const data = await response.json();
-    return NextResponse.json(data.contract[0].schema, { status: 200 });
+      const data = await response.json();
+      return NextResponse.json(data.contract[0].schema, { status: 200 });
+    }
+    if (network == 'eth' && chain == 'mainnet') {
+      const response = await fetch(
+        `https://api.etherscan.io/api?module=contract&action=getabi&address=${account}&apikey=${process.env.ETH_SCAN_API}`
+      );
+
+      const data = await response.json();
+      return NextResponse.json(JSON.parse(data.result), { status: 200 });
+    }
   } catch (e: any) {
     return NextResponse.json({ error: e.message }, { status: e.status ?? 500 });
   }
